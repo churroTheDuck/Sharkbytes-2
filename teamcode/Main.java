42720894683f4b5655970f20dcd0f6ac5c70ea35
royspro <154406790+royspro@users.noreply.github.com>
2024-11-22 21:58:40 -0800

    Update Main.java
    added grabservohandler stuff to main

--- teamcode/Main.java
+++ teamcode/Main.java
@@ -3,17 +3,24 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+/*
+config stuff:
+- motor arm is a dc motor under "motorArm"
+- the grabbing servo is under "grabServo"
+ */
+
 // main op mode
 @TeleOp
 public class Main extends OpMode {
     // handlers
     DrivingHandler    drivingHandler;
+    GrabServoHandler  grabServoHandler;
 
     // initialization
     public void init() {
         // setup handlers
         drivingHandler    = new DrivingHandler(hardwareMap);
-
+        grabServoHandler  = new GrabServoHandler(hardwareMap);
         telemetry.addData("status", "initialized");
         telemetry.update();
     }
@@ -22,7 +29,7 @@ public class Main extends OpMode {
     public void loop() {
         // run handlers
         drivingHandler.loop(gamepad1);
-
+        grabServoHandler.loop(gamepad1);
         telemetry.addData("status", "running");
         telemetry.update();
     }
