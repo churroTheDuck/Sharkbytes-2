abb9a11b6cfa5a1fbcee32faca6eba3eaee95d16
Ryo Sprosts <154406790+royspro@users.noreply.github.com>
2024-12-19 16:55:47 -0800

    Update DrivingHandler.java

--- teamcode/DrivingHandler.java
+++ teamcode/DrivingHandler.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Gamepad;
+
 
 // driving handler
 public class DrivingHandler {
@@ -19,6 +22,9 @@ public class DrivingHandler {
     DcMotor motorArm;
     
     CRServo grabServo;
+    
+    boolean holdArm;
+    boolean prevB;
 
     // initialization
     public DrivingHandler(HardwareMap hardwareMap) {
@@ -34,30 +40,58 @@ public class DrivingHandler {
         motorArm = hardwareMap.get(DcMotor.class, "motorArm");
         
         grabServo = hardwareMap.get(CRServo.class, "grabServo");
+        
+        holdArm = false;
+        prevB = false;
     }
 
     // gameplay loop
-    public void loop(Gamepad gamepad1) {
+    public void loop(Gamepad gamepad1, Telemetry telemetry) {
         float leftStickX  = gamepad1.left_stick_x;
         float leftStickY  = gamepad1.left_stick_y;
         float rightStickX = gamepad1.right_stick_x;
         float rightStickY = gamepad1.right_stick_y;
         float leftTrigger = gamepad1.left_trigger;
         float rightTrigger = gamepad1.right_trigger;
+        boolean up = gamepad1.dpad_up;
+        boolean down = gamepad1.dpad_down;
 
-        float moveX = leftStickX;
-        float moveY = rightTrigger-leftTrigger;
         float rot = rightStickX;
-        float arm = rightStickY / (float) 1.75;
+        float arm = rightTrigger - leftTrigger / (float) 1.0 + (float) 0.0;
         
+        if (gamepad1.b & !prevB) {
+            holdArm = !holdArm;
+        }
+        
+        if (holdArm) {
+            arm = (float) 0.24;
+        }
+        
+        telemetry.addData("arm", arm);
         
         double power = 1;
  
         // set motor powers
-        double frontLeft = (moveX - moveY + rot)*power;
-        double frontRight = (-moveX - moveY - rot)*power;
-        double backLeft = (-moveX - moveY + rot)*power;
-        double backRight = (moveX - moveY - rot)*power;
+        double frontLeft = (leftStickY+leftStickX)*power;
+        double frontRight = (rightStickY-leftStickX)*power;
+        double backLeft = (leftStickY-leftStickX)*power;
+        double backRight = (rightStickY+leftStickX)*power;
+        
+        
+        grabServo.setPower((rightStickX)*power);
+        float y = gamepad1.y ? 1 : 0;
+        float a = gamepad1.a ? 1 : 0;
+        double servoSpeed = y - a;
+        //grabServo.setPower(servoSpeed);
+        telemetry.addData("servo", servoSpeed);
+        
+        // if (gamepad1.y) {
+        //      grabServo.setPower(1.0);
+        // } else if (gamepad1.a) {
+        //      grabServo.setPower(-1.0);
+        // } else {
+        //      grabServo.setPower(0.0);
+        // }
 
         double max = Math.max(frontLeft, frontRight);
         max = Math.max(max, backLeft);
@@ -78,7 +112,6 @@ public class DrivingHandler {
         // arm
         motorArm.setPower(arm);
         
-        grabServo.setPower(-leftStickY);
-
+        prevB = gamepad1.b;
     }
 }
