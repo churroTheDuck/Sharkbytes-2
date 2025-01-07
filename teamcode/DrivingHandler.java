52e6832e0c4d392b3f96429dec4ac0950b793d5c
Ryo Sprosts <rsprosts@gmail.com>
2025-01-06 22:59:38 -0800

    [work in progress] added an auto file and a couple other bits
    deleted like one thing in driving handler
    did mostly thinking not coding for auto
    please ask more during ftc i dont feel like typing everything
    
    much testing needed

--- teamcode/DrivingHandler.java
+++ teamcode/DrivingHandler.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
